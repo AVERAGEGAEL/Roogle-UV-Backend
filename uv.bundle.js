44c9c259cf9256a0246910ba99cdca4adae91e4c
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-06-03 14:25:08 -0700

    Update uv.bundle.js

--- uv.bundle.js
+++ uv.bundle.js
@@ -1,15 +1,4 @@
-self.Ultraviolet = {
-  codec: {
-    xor: {
-      encode(url) {
-        return btoa(url.split('').map(c => String.fromCharCode(c.charCodeAt(0) ^ 2)).join(''));
-      },
-      decode(url) {
-        return atob(url).split('').map(c => String.fromCharCode(c.charCodeAt(0) ^ 2)).join('');
-      },
-    }
-  },
-};
-
-// UV Service Handler stub (needed so things don't break)
-if (!self.addEventListener) self.addEventListener = () => {};
+// Minimal UV core loader
+importScripts(
+  'https://unpkg.com/ultraviolet@latest/dist/uv.bundle.js'
+);
