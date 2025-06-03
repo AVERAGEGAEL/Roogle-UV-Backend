8d0ee73569fca53edf2427c1e6633c6312ad3893
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-06-03 14:55:09 -0700

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,24 +1,6 @@
 self.__uv$config = {
   prefix: '/service/',
-  bare: 'https://ultra-gael.uraverageopdoge.workers.dev/service/',
-  encodeUrl: (input) => self.__uv$config.prefix + self.__uv$config.codec.encode(input),
-  codec: {
-    xorKey: 0x55,
-    encode: function (str) {
-      const arr = [];
-      for (let i = 0; i < str.length; i++) {
-        arr.push((str.charCodeAt(i) ^ this.xorKey).toString(16).padStart(2, '0'));
-      }
-      return arr.join('');
-    },
-    decode: function (str) {
-      let result = '';
-      for (let i = 0; i < str.length; i += 2) {
-        const hex = str.slice(i, i + 2);
-        const charCode = parseInt(hex, 16) ^ this.xorKey;
-        result += String.fromCharCode(charCode);
-      }
-      return result;
-    }
-  }
+  bare: 'https://ultra-gael.uraverageopdoge.workers.dev/',
+  encodeUrl: (input) => __uv$config.prefix + Ultraviolet.codec.xor.encode(input),
+  codec: Ultraviolet.codec.xor
 };
