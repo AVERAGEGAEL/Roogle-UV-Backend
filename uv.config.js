0bf1a011ac2516ebe79be3c6dbbef852f6c3ca79
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-06-03 14:25:23 -0700

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,10 +1,6 @@
 self.__uv$config = {
   prefix: '/service/',
   bare: 'https://ultra-gael.uraverageopdoge.workers.dev/',
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: '/uv.handler.js',
-  bundle: '/uv.bundle.js',
-  config: '/uv.config.js',
-  sw: '/uv.sw.js',
+  encodeUrl: (input) => __uv$config.prefix + __uv$config.codec.encode(input),
+  codec: Ultraviolet.codec.xor
 };
