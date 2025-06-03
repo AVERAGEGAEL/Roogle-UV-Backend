33d558e416ff74822b9fc7702c77cdf026ca12a3
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-06-03 14:34:33 -0700

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/service/',
-  bare: 'https://ultra-gael.uraverageopdoge.workers.dev/',
+  bare: 'https://averagegael.github.io/Roogle-UV-Backend/',  // <-- Changed from Worker to GitHub Pages
   encodeUrl: (input) => __uv$config.prefix + __uv$config.codec.encode(input),
   codec: Ultraviolet.codec.xor
 };
