afbf0fdcef03bab8528918dfe3f7cca2fb409ea2
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-06-03 11:49:21 -0700

    Create uv.config.js

---
+++ uv.config.js
@@ -0,0 +1,6 @@
+self.__uv$config = {
+  prefix: '/uv/service/',
+  bare: 'https://ultra-gael.uraverageopdoge.workers.dev',
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+};
