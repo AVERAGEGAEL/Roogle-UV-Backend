16cda5c3e5169d83634a4d196e4d7bb920235a88
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-06-03 14:55:28 -0700

    Update uv.bundle.js

--- uv.bundle.js
+++ uv.bundle.js
@@ -1,4 +1,7 @@
-// Minimal UV core loader
-importScripts(
-  'https://unpkg.com/ultraviolet@latest/dist/uv.bundle.js'
-);
+// uv.bundle.js
+// Proxy to CDN-hosted UV core
+
+// This file just loads UV core from CDN dynamically
+const script = document.createElement('script');
+script.src = 'https://unpkg.com/ultraviolet@latest/dist/uv.bundle.js';
+document.head.appendChild(script);
