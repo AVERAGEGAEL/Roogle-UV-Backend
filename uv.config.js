2a71cda7a2a0811cb70dbcbdeb9a6c1f808aa618
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-06-05 08:47:22 -0700

    Delete uv.config.js

--- uv.config.js
+++
@@ -1,6 +0,0 @@
-self.__uv$config = {
-  prefix: '/service/',
-  bare: 'https://ultra-gael.uraverageopdoge.workers.dev/',
-  encodeUrl: (input) => __uv$config.prefix + Ultraviolet.codec.xor.encode(input),
-  codec: Ultraviolet.codec.xor
-};
