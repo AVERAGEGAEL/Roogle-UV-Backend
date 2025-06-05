bb9b694b1f6f0ec37f605cd76ed44982b3f07cc1
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-06-05 08:47:11 -0700

    Delete uv.bundle.js

--- uv.bundle.js
+++
@@ -1,7 +0,0 @@
-// uv.bundle.js
-// Proxy to CDN-hosted UV core
-
-// This file just loads UV core from CDN dynamically
-const script = document.createElement('script');
-script.src = 'https://unpkg.com/ultraviolet@latest/dist/uv.bundle.js';
-document.head.appendChild(script);
