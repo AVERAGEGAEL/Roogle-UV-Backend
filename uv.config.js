859d78947ff7eae85eb312aab982ba9015eba8c2
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-06-11 10:38:15 -0700

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-  prefix: '/Roogle-UV-Backend/',
+  prefix: '/Roogle-UV-Backend/service/',
   bare: 'https://incog.dev/bare/',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
